81e9a155992191a27b78b9e666803ff8386b881f
stemen23daw2 <15585938.clot@fje.edu>
2023-09-20 18:52:48 +0200

    Commit 2 del projecte loginteller

--- loginteller.c
+++ loginteller.c
@@ -1,20 +1,20 @@
 /* loginteller.c
  * Based on monousuar.c at http://www.binefa.net/gnu/gcc/processos/Informacio_d_usuari.html
  * www.binefa.cat
- * 20120507
+ * 20230920
 */
 #include <stdio.h>
 #include <unistd.h>    // getlogin()
 #include <stdlib.h>    // exit()
 
 int main(){
-    char *szLogin;
+    char *szUserName;
    
-    if((szLogin = getlogin())==NULL){
+    if((szUserName = getlogin())==NULL){
         perror("getlogin");
         exit(EXIT_FAILURE);
     }  
-    printf("Your login name at the system is : %s\n",szLogin);
+    printf("Your login name at the system is : %s\n",szUserName);
 
     return (0);
 }
